887c2e7dfdf9d3217c40ae1f496e55fcab0755ad
xuewueagle <xuewueagle@163.com>
2025-01-05 00:19:08 +0800

    基于maven创建了4个子模块做为4个微服务

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# JavaArchive
-Java代码实践、积累
+# JavaArchive的用途
+> Java代码实践、积累
+# 分支说明
+- master分支：JDK 8 + Spring Boot 2.7
+- master-jdk17分支：JDK 17 + Spring Boot 3.2
+- master-jdk21分支：JDK 21 + Spring Boot 3.2
+# 项目介绍
+## 1、 java8-basic
+> 积累java8常用特性
+
+## 2、eagle-springboot-app
+> 基于SpringBoot实现多模块服务开发，并持续完善及优化
+
+## 3、eagle-springcloud-app
+> 基于SpringBoot+SpringCloud Alibaba实现微服务开发，并持续完善及优化
---
+++ eagle-springcloud-app/eagle-gateway/src/main/java/com/eagle/App.java
@@ -0,0 +1,25 @@
+package com.eagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+//public class App
+//{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+//}
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
